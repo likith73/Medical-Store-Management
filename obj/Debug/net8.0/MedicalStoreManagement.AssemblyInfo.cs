0ee5f223bfb93cb7924eda3e9b719a5e2928d490
Likhit <venkatasaikrishna.thatavarthi@slu.edu>
2025-09-03 13:48:20 -0500

    Updated program.cs and Added MedicalContext.cs

--- obj/Debug/net8.0/MedicalStoreManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedicalStoreManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalStoreManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b3721b99daac3fb52b459e5f8a3f1c595d00af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18fcc066144f653dee641594f4aa1f3fb658ff9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalStoreManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicalStoreManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
